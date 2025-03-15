d43e342967d5d8fd2f3fa1b24d238235c0175358
Arpit <omagrahari55@gmail.com>
2025-03-16 00:07:10 +0530

    added the footer comp

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '@/Components/Navbar/Navbar'
 import Section1 from '@/Components/Section1/Section1.jsx'
+import Footer from '@/Components/Footer/footer'
 const page = () => {
   return (
     <>
       <Navbar/>
       <Section1/>
+      <Footer/>
     </>
     )
 }
